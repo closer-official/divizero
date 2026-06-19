6e6a0db87a3dcac4d2459e6620398569d006b8ac
Closer Official <info@closer-official.com>
2026-06-19 12:07:32 +0900

    deployメッセージ引数修正

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process');
-const message = process.env.npm_config_m || 'update';
+const message = process.argv[2] || 'update';
 execSync('git add -A', { stdio: 'inherit' });
 execSync(`git commit -m "${message}"`, { stdio: 'inherit' });
 execSync('git push', { stdio: 'inherit' });
